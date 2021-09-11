c0c83a2456fa233a381c536c0e8b0c43a2a64379
mexane.delcroix <mexane.delcroix@epita.fr>
2021-09-11 17:08:01 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -9,16 +9,16 @@ if [[ $distrib = "ubuntu" ]]; then
     sudo apt-get install python-pygments
 elif [[ $distrib = "manjaro" ]]; then
     echo This use pacman
-    sudo pacman -S --no-confirm zsh
-    sudo pacman -S --no-confirm python-pygments
+    sudo pacman -S zsh
+    sudo pacman -S  python-pygments
 fi
 
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i -r 's+ZSH_THEME=".*"+ZSH_THEME="powerlevel10k/powerlevel10k"+g'  zsh/.zshrc
-mv zsh/.zshrc $HOME
-mv zsh/aliases.zsh $ZSH_CUSTOM
+cp zsh/.zshrc $HOME
+cp zsh/aliases.zsh $ZSH_CUSTOM
 source .zshrc
 
 wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
@@ -26,5 +26,3 @@ wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20R
 mkdir ~/.local/share/fonts
 mv *.ttf ~/.local/share/fonts
 fc-cache -f -v
-
-p10k configure
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -9,9 +9,7 @@ fi
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/home/mexane.delcroix/.oh-my-zsh"
-
-export PATH="$PATH:/usr/local/android-studio/bin"
+export ZSH="/home/kilehynn/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
